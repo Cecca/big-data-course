8c66082aaae993a192e879c2264f6a311062b028
Matteo Ceccarello <ceccarel@dei.unipd.it>
2017-11-06 11:45:28 +0100

    Initial commit of configuration

---
+++ cluster-setup/elastiq-conf.sh
@@ -0,0 +1,25 @@
+export FLAVOR=cloudveneto.medium
+export MAX_VMS=3
+export MIN_VMS=2
+export JOBS_PER_VM=2
+export IDLE_TIME=2400
+export KEY_NAME=cloudveneto-machines
+
+## EC2-id of the image to be used for minions. Uncomment your choice
+##
+##  Fedora 23:            
+#export IMAGE_ID=ami-00000027
+##  ubuntu-16.04.1-LTS:   
+export IMAGE_ID=ami-000000c6
+##  ubuntu-14.04.3-LTS:   
+#export IMAGE_ID=ami-0000001b
+##  uCernVM3:             
+#export IMAGE_ID=ami-0000005a
+##  CentOS 7:             
+#export IMAGE_ID=ami-00000013
+##  CentOS 6:             
+#export IMAGE_ID=ami-00000010
+##  Custom. You need to know the EC2-id of the image
+#export IMAGE_ID=
+
+
