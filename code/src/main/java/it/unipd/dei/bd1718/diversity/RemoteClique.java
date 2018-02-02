0f3b486ff8c90670f0fbf836865800546fec91d5
Matteo Ceccarello <ceccarel@dei.unipd.it>
2018-02-02 12:51:04 +0100

    Repartition data across available cores

--- code/src/main/java/it/unipd/dei/bd1718/diversity/RemoteClique.java
+++ code/src/main/java/it/unipd/dei/bd1718/diversity/RemoteClique.java
@@ -184,12 +184,27 @@ public class RemoteClique {
     SparkConf conf = new SparkConf(true)
             .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
             .setAppName("Remote clique diversity");
+    logger.info("Provided configuration: {}", conf.toDebugString());
 
     JavaSparkContext sc = new JavaSparkContext(conf);
 
-    long startTime = System.currentTimeMillis();
+    int executorCores = conf.getInt("spark.executor.cores", -1);
+    int numExecutors = conf.getInt("spark.executor.instances", -1);
+    int numPartitions = sc.defaultParallelism();
+    if (executorCores > 0 && numExecutors > 0) {
+      numPartitions = executorCores * numExecutors;
+    } else {
+      logger.warn("Could not determine number of executors or cores: {} and {}, respectively",
+              executorCores, numExecutors);
+      logger.warn("Configuration: ", conf.toDebugString());
+    }
 
-    JavaRDD<Tuple2<Long, Vector>> vectors = InputOutput.readVectorsPairs(sc, arguments.input);
+    long startTime = System.currentTimeMillis();
+    logger.info("Start time: {}", startTime);
+    logger.info("Number of partitions: {}", numPartitions);
+
+    JavaRDD<Tuple2<Long, Vector>> vectors = InputOutput.readVectorsPairs(sc, arguments.input)
+            .repartition(numPartitions);
 
     ArrayList<Tuple2<Long, Vector>> solution;
     if ("random".equals(arguments.algorithm)) {
@@ -242,7 +257,7 @@ public class RemoteClique {
 
     long endTime = System.currentTimeMillis();
 
-    logger.info("Elapsed time: {} ms", endTime - startTime);
+    System.out.println("Elapsed time: " + (endTime - startTime) + " ms");
 
   }
 
