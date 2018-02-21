59606100305eca214f8766bfb9d2ba76325136a3
Matteo Ceccarello <ceccarel@dei.unipd.it>
2018-02-21 16:47:38 +0100

    Simplify the lemmatizer

--- code/src/main/java/it/unipd/dei/bd1718/admin/Lemmatizer.java
+++ code/src/main/java/it/unipd/dei/bd1718/admin/Lemmatizer.java
@@ -1,13 +1,21 @@
 package it.unipd.dei.bd1718.admin;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -38,50 +46,61 @@ public class Lemmatizer {
     pipeline = new StanfordCoreNLP(props);
   }
 
-  /**
-   * Some symbols are interpreted as tokens. This regex allows us to exclude them.
-   */
-  private static Pattern symbols = Pattern.compile("^[',\\.`/-_]+$");
-
-  /**
-   * A set of special tokens that are present in the Wikipedia dataset, and that should be removed
-   */
-  private static HashSet<String> specialTokens =
-          new HashSet<>(Arrays.asList("-lsb-", "-rsb-", "-lrb-", "-rrb-", "'s", "--"));
-
-  public static ArrayList<ArrayList<String>> lemmatizedSentences(String doc) {
+  public static String lemmatize(String doc) {
     long start = System.currentTimeMillis();
     Annotation document = new Annotation(doc);
     pipeline.annotate(document);
 
-    ArrayList<ArrayList<String>> sentences = new ArrayList<>();
+    StringBuffer result = new StringBuffer();
 
     long numLemmas = 0;
-    for (CoreMap sentence : document.get(CoreAnnotations.SentencesAnnotation.class)) {
-      ArrayList<String> lemmas = new ArrayList<>();
-      for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-        String lemma = token.get(CoreAnnotations.LemmaAnnotation.class);
-        numLemmas++;
-        // Remove symbols
-        if (!symbols.matcher(lemma).matches() && !specialTokens.contains(lemma)) {
-          lemmas.add(lemma);
-        }
-      }
-      if (!lemmas.isEmpty()) {
-        sentences.add(lemmas);
-      }
+    for (CoreLabel token : document.get(CoreAnnotations.TokensAnnotation.class)) {
+      String lemma = token.get(CoreAnnotations.LemmaAnnotation.class);
+      numLemmas++;
+      result.append(lemma);
+      result.append(' ');
     }
     long end = System.currentTimeMillis();
     logger.info("Performed initial annotation in " + (end - start) + " ms");
     double throughput = 1000 * ((double) numLemmas) / (end - start);
     logger.info("Annotation throughput " + throughput + " lemmas/s");
 
-    return sentences;
+    return result.toString();
   }
 
-  public static void main(String[] args) {
-    System.out.println(lemmatizedSentences(
-            "This is a sentence. This is another. The whole thing is a document made of sentences."));
+  private static class Args {
+
+    @Parameter(names = "--input", required = true, description = "Path to the input dataset")
+    String input;
+
+    @Parameter(names = "--output", required = true, description = "Path to the output vector dataset")
+    String output;
+
+  }
+
+  public static void main(String[] args) throws IOException {
+    Args arguments = new Args();
+    JCommander.newBuilder()
+            .addObject(arguments)
+            .build()
+            .parse(args);
+
+    SparkConf conf = new SparkConf(true)
+            .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+            .setAppName("Simple preprocessing");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+    logger.info(sc.getConf().toDebugString());
+    FileSystem fs = FileSystem.get(sc.hadoopConfiguration());
+
+    // Verify command line arguments
+    if (fs.exists(new Path(arguments.output))) {
+      logger.error("output already exists, not overwriting");
+      return;
+    }
+
+    WikiPage.readPages(sc, arguments.input)
+            .map((page) -> lemmatize(page.getText()))
+            .saveAsTextFile(arguments.output);
   }
 
 }
