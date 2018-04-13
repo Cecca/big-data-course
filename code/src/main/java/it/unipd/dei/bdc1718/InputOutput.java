3271edf7bba97032a0d9f95129303ba1338e041f
Matteo Ceccarello <ceccarel@dei.unipd.it>
2018-04-13 18:14:56 +0200

    Move binary methods to BinaryIO

--- code/src/main/java/it/unipd/dei/bdc1718/InputOutput.java
+++ code/src/main/java/it/unipd/dei/bdc1718/InputOutput.java
@@ -1,16 +1,10 @@
 package it.unipd.dei.bdc1718;
 
-import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.hadoop.io.compress.BZip2Codec;
-import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.io.CompressionCodec;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
-import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.execution.datasources.OutputWriter;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -74,47 +68,6 @@ public class InputOutput {
     }
   }
 
-  public static JavaRDD<Vector> readVectorsBin(JavaSparkContext sc, String path) {
-    return new SparkSession(sc.sc())
-            .read()
-            .parquet(path)
-            .as(Encoders.kryo(Vector.class))
-            .javaRDD();
-  }
 
-  public static void writeVectorsBin(JavaRDD<Vector> vectors, String path) {
-    new SparkSession(vectors.context())
-            .createDataset(vectors.rdd(), Encoders.kryo(Vector.class))
-            .write()
-            .option("compression", "gzip")
-            .parquet(path);
-  }
-
-  public static ArrayList<Vector> readVectorsSequentialBin(String path) {
-    SparkConf conf = new SparkConf().setMaster("local").setAppName("parquet reading");
-    JavaSparkContext sc = new JavaSparkContext(conf);
-    sc.setLogLevel("ERROR");
-    ArrayList<Vector> data = new ArrayList<>();
-    data.addAll(readVectorsBin(sc, path).collect());
-    return data;
-  }
-
-  public static void main(String[] args) throws IOException {
-    String subcmd = args[0];
-    String input = args[1];
-    String output = args[2];
-
-    SparkConf conf = new SparkConf(true).setAppName("conversions");
-    JavaSparkContext sc = new JavaSparkContext(conf);
-
-    if ("bin2txt".equals(subcmd)) {
-      writeVectors(readVectorsBin(sc, input), output);
-    } else if ("txt2bin".equals(subcmd)) {
-      writeVectorsBin(readVectors(sc, input), output);
-    } else {
-      throw new IllegalArgumentException("Unknown subcommand");
-    }
-
-  }
 
 }
---
+++ code/src/main/java/it/unipd/dei/bdc1718/admin/BinaryIO.java
@@ -0,0 +1,58 @@
+package it.unipd.dei.bdc1718.admin;
+
+import it.unipd.dei.bdc1718.InputOutput;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.SparkSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class BinaryIO {
+
+  public static JavaRDD<Vector> readVectorsBin(JavaSparkContext sc, String path) {
+    return new SparkSession(sc.sc())
+            .read()
+            .parquet(path)
+            .as(Encoders.kryo(Vector.class))
+            .javaRDD();
+  }
+
+  public static void writeVectorsBin(JavaRDD<Vector> vectors, String path) {
+    new SparkSession(vectors.context())
+            .createDataset(vectors.rdd(), Encoders.kryo(Vector.class))
+            .write()
+            .option("compression", "gzip")
+            .parquet(path);
+  }
+
+  public static ArrayList<Vector> readVectorsSequentialBin(String path) {
+    SparkConf conf = new SparkConf().setMaster("local").setAppName("parquet reading");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+    sc.setLogLevel("ERROR");
+    ArrayList<Vector> data = new ArrayList<>();
+    data.addAll(readVectorsBin(sc, path).collect());
+    return data;
+  }
+
+  public static void main(String[] args) throws IOException {
+    String subcmd = args[0];
+    String input = args[1];
+    String output = args[2];
+
+    SparkConf conf = new SparkConf(true).setAppName("conversions");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+
+    if ("bin2txt".equals(subcmd)) {
+      InputOutput.writeVectors(readVectorsBin(sc, input), output);
+    } else if ("txt2bin".equals(subcmd)) {
+      writeVectorsBin(InputOutput.readVectors(sc, input), output);
+    } else {
+      throw new IllegalArgumentException("Unknown subcommand");
+    }
+
+  }
+}
