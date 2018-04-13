4172285e9ac055661c7b7231d75b4f370bcf39df
Matteo Ceccarello <ceccarel@dei.unipd.it>
2018-04-13 16:09:08 +0200

    Simplify third homework template

--- code/src/main/java/it/unipd/dei/bdc1718/ThirdHomework.java
+++ code/src/main/java/it/unipd/dei/bdc1718/ThirdHomework.java
@@ -1,14 +1,8 @@
 package it.unipd.dei.bdc1718;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 
 public class ThirdHomework {
@@ -21,46 +15,28 @@ public class ThirdHomework {
     return Math.sqrt(Vectors.sqdist(a, b));
   }
 
-  private static class Args {
-
-    @Parameter(names = "--input", required = true, description = "Path to the input dataset")
-    String input;
-
-    @Parameter(names = "-k", required = true)
-    int k;
-
-  }
-
-  private static void appendResult(Args arguments, long elapsedTime) throws IOException {
-    Files.write(
-            Paths.get("k-center-time.txt"),
-            (arguments.input + "," + arguments.k + "," + elapsedTime + "\n").getBytes(),
-            StandardOpenOption.APPEND,
-            StandardOpenOption.CREATE);
-  }
-
   public static void main(String[] args) throws Exception {
 
-    Args arguments = new Args();
-    JCommander.newBuilder()
-            .addObject(arguments)
-            .build()
-            .parse(args);
+    if (args.length != 2) {
+      throw new IllegalArgumentException("USAGE: ThirdHomework PATH K");
+    }
 
-    ArrayList<Vector> input = InputOutput.readVectorsSequential(arguments.input);
+    String inputPath = args[0];
+    int k = Integer.parseInt(args[1]);
+
+    ArrayList<Vector> input = InputOutput.readVectorsSequential(inputPath);
     long start = System.currentTimeMillis();
     System.out.println("Loaded input with " + input.size() + " points");
-    ArrayList<Vector> solution = kCenter(input, arguments.k);
+    ArrayList<Vector> solution = kCenter(input, k);
     long end = System.currentTimeMillis();
 
-    if (solution.size() > arguments.k) {
+    if (solution.size() > k) {
       throw new IllegalArgumentException(
-              "The solution has " + solution.size() + " points ( > " + arguments.k + " )");
+              "The solution has " + solution.size() + " points ( > " + k + " )");
     }
 
     long elapsed = end - start;
     System.out.println("Elapsed time " + elapsed + " ms");
-    appendResult(arguments, elapsed);
   }
 
 
